bd791a3c9cbe4c568a45cc806eca5068bd30caf8
Yuttasak(Yo)Pannawat <yuttasakcom@gmail.com>
2020-01-25 22:54:16 +0700

    KOA-EFK Sample

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import koa from 'koa'
+import bodyParser from 'koa-bodyparser'
 
 import fluentLogger from './middleware/fluent-logger'
 
 const app = new koa()
 const PORT = process.env.PORT || '3000'
 
+app.use(bodyParser())
 app.use(fluentLogger())
 
 app.use(async ctx => {
-  ctx.logger.emit('follow', { message: 'Yo ........' })
+  ctx.logger.emit('appLog', { datas: JSON.stringify({ msg: 'Yo :)' }) })
 })
 
 app.listen(PORT, () => {
--- src/middleware/fluent-logger.js
+++ src/middleware/fluent-logger.js
@@ -1,6 +1,7 @@
 import logger from 'fluent-logger'
 
 const FLUENT_PREFIX = 'fluentd.test'
+const ACCESS_LOG = 'accessLog'
 const FLUENT_HOST = {
   host: 'localhost',
   port: 24224,
@@ -12,7 +13,25 @@ export default (prefix = FLUENT_PREFIX, host = FLUENT_HOST) => {
   logger.configure(prefix, host)
 
   return async (ctx, next) => {
+    const { req, request, res } = ctx
+    let start = new Date()
+
+    const logObject = {
+      timestamp: start.getTime(),
+      'remote-address': ctx.ip,
+      method: req.method,
+      host: req.headers.host,
+      url: req.url,
+      'http-version': req.httpVersion,
+      status: res.statusCode,
+      'response-time': new Date() - start,
+      'request-headers': JSON.stringify(req.headers),
+      'request-body': JSON.stringify(request.body),
+    }
+
+    logger.emit(ACCESS_LOG, logObject)
     ctx.logger = logger
+
     next()
   }
 }
